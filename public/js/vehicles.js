a1384b18c855a77441c5f4e4729c812d9bee3846
Houdychou <hchouchouh@edenschool.fr>
2025-04-14 17:37:11 +0200

    vehicle page done

---
+++ public/js/vehicles.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("a.custom-link");
+
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const url = link.getAttribute("href");
+
+            fetch(url, {
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            })
+                .then(response => response.json())
+                .then(vehicules => {
+                    const container = document.getElementById('vehicle-list');
+                    container.innerHTML = '';
+
+                    vehicules.forEach(v => {
+                        const card = `
+                                <div class="card-cars">
+                                    <img src="/${v.image_url}" class="vehicle-img" alt="${v.brand} ${v.model}">
+                                    <div class="container-cars">
+                                        <div class="top-content">
+                                            <div class="content-cars">
+                                                <h3>${v.brand} ${v.model}</h3>
+                                                <span class="grey-span">${capitalizeFirstLetter(v.name)}</span>
+                                            </div>
+                                            <div class="content-cars">
+                                                <span class="price">$${v.price_per_day}</span>
+                                                <span class="grey-span">per day</span>
+                                            </div>
+                                        </div>
+                                        <div class="vehicle-details">
+                                            <div class="details">
+                                                <img src="/images/icons/transmission-icon.svg" class="icons" alt="Transmission">
+                                                <span class="grey-span">${capitalizeFirstLetter(v.transmission)}</span>
+                                            </div>
+                                            <div class="details">
+                                                <img src="/images/icons/fuel-icon.svg" class="icons" alt="Fuel">
+                                                <span class="grey-span">${capitalizeFirstLetter(v.fuel_type)}</span>
+                                            </div>
+                                            <div class="details">
+                                                <img src="/images/icons/air-conditionner.svg" class="icons" alt="AC">
+                                                <span class="grey-span">${v.air_conditioning ? 1 : "Air Conditioner"}</span>
+                                            </div>
+                                        </div>
+                                        <a href="/vehicules/${v.id}" class="details-btn">View Details</a>
+                                    </div>
+                                </div>
+                            `;
+                        container.innerHTML += card;
+                    });
+                })
+                .catch(error => console.error("Erreur AJAX :", error));
+        });
+    });
+});
+
+function capitalizeFirstLetter(val) {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
---
+++ resources/views/vehicule.blade.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Tous les véhicules">
+    <title>Page des véhicules</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/vehicule.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
+</head>
+
+<body>
+<div class="container">
+    <header>
+        <div class="header-container">
+            <div class="car-rental">
+                <img src="{{ asset('images/icons/car-logo.svg') }}" alt="car logo">
+                <p>Car Rental</p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="/" class="home">Home</a></li>
+                    <li><a href="/vehicules" class="vehicles">Vehicles</a></li>
+                    <li><a href="/details" class="details">Details</a></li>
+                    <li><a href="/about" class="about">About us</a></li>
+                </ul>
+            </nav>
+            <div class="help">
+                <img src="{{ asset('images/icons/phone-icon.svg') }}" alt="phone image">
+                <div class="help-content">
+                    <p class="need-help">Need help?</p>
+                    <p class="phone-number">+996 247-1680</p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="select-content">
+            <h1>Select a vehicle group</h1>
+
+            <div class="type-vehicle content-vehicle">
+                <a class="vehicle-type purple" href="/vehicules">All Vehicles</a>
+                @foreach($typeVehicules as $type)
+                    <a class="custom-link"
+                       href="/vehicules/type/{{ urlencode($type->name) }}">{{ Str::title($type->name) }}</a>
+                @endforeach
+            </div>
+
+            <div class="fuel-vehicle content-vehicle">
+                <a class="energy-type purple" href="/vehicules">All Energy Type</a>
+                @foreach($fuelTypes as $fuel)
+                    <a class="custom-link"
+                       href="/vehicules/energy/{{ urlencode($fuel->fuel_type) }}">{{ Str::title($fuel->fuel_type) }}</a>
+                @endforeach
+            </div>
+
+            <div class="gear-vehicle content-vehicle">
+                <a class="gear-type purple" href="/vehicules">All Type Of Gear</a>
+                @foreach($gearType as $gear)
+                    <a class="custom-link"
+                       href="/vehicules/gear/{{ urlencode($gear->transmission) }}">{{ Str::title($gear->transmission) }}</a>
+                @endforeach
+            </div>
+        </div>
+        <div class="cars">
+            <div class="grid-cars" id="vehicle-list">
+                @foreach($vehicules as $vehicule)
+                    <div class="card-cars">
+                        <img src="{{ asset($vehicule->image_url) }}"
+                             class="vehicle-img"
+                             alt="{{ $vehicule->brand . " " . $vehicule->model }}">
+                        <div class="container-cars">
+                            <div class="top-content">
+                                <div class="content-cars">
+                                    <h3>{{ $vehicule->brand . " " . $vehicule->model }}</h3>
+                                    <span class="grey-span">{{ Str::title($vehicule->name) }}</span>
+                                </div>
+                                <div class="content-cars">
+                                    <span class="price">${{ $vehicule->price_per_day }}</span>
+                                    <span class="grey-span">per day</span>
+                                </div>
+                            </div>
+                            <div class="vehicle-details">
+                                <div class="details">
+                                    <img src="{{ asset('images/icons/transmission-icon.svg') }}" class="icons"
+                                         alt="type of transmission image">
+                                    <span class="grey-span">{{ Str::title($vehicule->transmission) }}</span>
+                                </div>
+                                <div class="details">
+                                    <img src="{{ asset('images/icons/fuel-icon.svg') }}" class="icons" alt="fuel image">
+                                    <span class="grey-span">{{ Str::title($vehicule->fuel_type) }}</span>
+                                </div>
+                                <div class="details">
+                                    <img src="{{ asset('images/icons/air-conditionner.svg') }}" class="icons"
+                                         alt="conditionner image">
+                                    @if($vehicule->air_conditioning === 1)
+                                        <span class="grey-span">Air Conditioner</span>
+                                    @else
+                                        <span class="grey-span">None</span>
+                                    @endif
+                                </div>
+                            </div>
+                            <a href="/vehicules/{{ $vehicule->id }}" class="details-btn">View Details</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="brands">
+            <img src="{{ asset("images/logo/toyota.png") }}" alt="toyota">
+            <img src="{{ asset("images/logo/ford.png") }}" alt="ford">
+            <img src="{{ asset("images/logo/mercedes.png") }}" alt="mercedes">
+            <img src="{{ asset("images/logo/jeep.png") }}" alt="jeep">
+            <img src="{{ asset("images/logo/bmw.png") }}" alt="bmw">
+            <img src="{{ asset("images/logo/audi.png") }}" alt="audi">
+        </div>
+    </main>
+
+    <footer>
+        <div class="footer-content">
+            <div class="car-rental">
+                <img src="{{ asset('images/icons/car-logo.svg') }}" alt="car logo">
+                <p>Car Rental</p>
+            </div>
+            <div class="address card-footer">
+                <img src="{{ asset('images/icons/location-footer.svg') }}" alt="location image">
+                <div class="content">
+                    <p>Address</p>
+                    <p class="bold-text">Oxford Ave. Cary, NC 27511</p>
+                </div>
+            </div>
+            <div class="email card-footer">
+                <img src="{{ asset('images/icons/email-footer.svg') }}" alt="email image">
+                <div class="content">
+                    <p>Email</p>
+                    <p class="bold-text">nwiger@yahoo.com</p>
+                </div>
+            </div>
+            <div class="phone card-footer">
+                <img src="{{ asset('images/icons/phone-footer.svg') }}" alt="phone image">
+                <div class="content">
+                    <p>Phone</p>
+                    <p class="bold-text">+537 547-6401</p>
+                </div>
+            </div>
+        </div>
+        <p class="copy">&copy; Copyright Car Rental 2024. Made by Houdeyfa</p>
+    </footer>
+</div>
+
+<script src="{{ asset('js/vehicles.js') }}"></script>
+</body>
+</html>
